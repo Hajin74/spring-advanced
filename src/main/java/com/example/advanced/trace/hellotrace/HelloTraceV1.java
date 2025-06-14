b5b832c01cfcb927a9bea587ba8fded096ab6f58
Hajin74 <qwertygoov@naver.com>
2025-06-14 14:35:53 +0900

    initial commit

---
+++ src/main/java/com/example/advanced/trace/hellotrace/HelloTraceV1.java
@@ -0,0 +1,26 @@
+package com.example.advanced.trace.hellotrace;
+
+import com.example.advanced.trace.TraceId;
+import com.example.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV1 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    public void end(TraceStatus status) {
+        
+    }
+}
